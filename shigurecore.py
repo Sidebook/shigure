7cf8fdbe6589c098f5ce481300e2ab8752fc7eeb
Wakimoto Ryu <sidebook37@gmail.com>
2017-07-24 03:56:00 +0900

    modified shigurecore.py

--- shigurecore.py
+++ shigurecore.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import settings
-import datetime
+import datetime as dt
 
 class Forecast:
 
@@ -125,8 +125,12 @@ class Forecast:
 
             # set time
             length = max(min(len(forecasts), length), 2)
-            self.time_start = forecasts[0]['fcst_valid_local']
-            self.time_end = forecasts[length - 1]['fcst_valid_local']
+            self.time_start = dt.datetime.strptime(
+                forecasts[0]['fcst_valid_local'],
+                '%Y-%m-%dT%H:%M:%S%z')
+            self.time_end = dt.datetime.strptime(
+                forecasts[length - 1]['fcst_valid_local'],
+                '%Y-%m-%dT%H:%M:%S%z')
 
             # set PoP
             pop = []
@@ -152,7 +156,7 @@ class Forecast:
                     if (pop > threshold):
                         break
                     hour += 1
-                self.rain_begin_time = hour
+                self.rain_begin_time = self.time_start + dt.timedelta(hours=hour)
             
 
 class Responce:
@@ -163,6 +167,7 @@ class Responce:
     NEED_UMBRELLA = 3
     NOT_NEED_UMBRELLA = 4
     INTERNAL_ERROR = 5
+    DETAIL = 6
 
     def __init__(self, message='',status=GREETING):
         self.message = message
@@ -174,21 +179,14 @@ class Responce:
 def responce(message, latitude=None, longitude=None):
     r = Responce()
     if '傘いる' in message:
-        print('responce mode: forecast')
         if latitude is None or longitude is None:
             return Responce(
-                message='まずあなたの居る場所を教えてください',
+                message='あなたの住んでいる場所を教えてください',
                 status=Responce.UNKOWN_LOCATION
-            )
-        if '明日' in message:
-            return Responce(
-                message='明日の天気はわかりません',
-                status=Responce.NOT_NEED_UMBRELLA
             )
         else:
             f = Forecast()
             f.get(latitude, longitude)
-            print(f)
             
             if (f.status != Forecast.OK):
                 return Responce(
@@ -205,27 +203,79 @@ def responce(message, latitude=None, longitude=None):
                 s = '今日はおそらく傘は必要ありません'
                 status = Responce.NOT_NEED_UMBRELLA
             elif f.rain_level == Forecast.RAIN_MAYBE:
-                s = '{}時間後に雨が降るかもしれませんので、傘が必要かもしれません。'.format(
-                    f.rain_begin_time
+                s = '{}時頃から雨が降る可能性があるので、傘が必要かもしれません。'.format(
+                    f.rain_begin_time.hour
                 )
             elif f.rain_level == Forecast.RAIN_LIKELY:
-                s = '{}時間後に雨が降る可能性が高いので、傘が必要です。'.format(
-                    f.rain_begin_time
+                s = '{}時頃から雨が降る可能性が高いので、傘が必要です。\n降水確率は最高で {}% です。'.format(
+                    f.rain_begin_time.hour,
+                    max(f.pop)
                 )
             elif f.rain_level == Forecast.RAIN_ALMOST:
-                s = '{}時間後に雨が高確率で降るので、傘が必要です。'.format(
-                    f.rain_begin_time
+                s = '{}時頃から雨がかなりの高確率で降るので、傘が必要です。\n降水確率は最高で {}% です。'.format(
+                    f.rain_begin_time.hour,
+                    max(f.pop)
                 )
             
             return Responce(
                 message=s,
                 status=status
             )
+    elif '詳細' in message:
+        f = Forecast()
+        f.get(latitude, longitude)
+        
+        if (f.status != Forecast.OK):
+            return Responce(
+                message='すいません...\nなにか問題が発生していてお答えできません...',
+                status=Responce.INTERNAL_ERROR
+            )
+        
+        s = ''
+        time = f.time_start
+
+        if f.rain_level == Forecast.RAIN_NEVER:
+            s = '今日は傘は必要ありません\n'
+        elif f.rain_level == Forecast.RAIN_HARDLY:
+            s = '今日はおそらく傘は必要ありません\n'
+        elif f.rain_level == Forecast.RAIN_MAYBE:
+            s = '{}時頃から雨が降る可能性があります。\n'.format(
+                f.rain_begin_time.hour
+            )
+        elif f.rain_level == Forecast.RAIN_LIKELY:
+            s = '{}時頃から雨が降る可能性が高いです。\n'.format(
+                f.rain_begin_time.hour
+            )
+        elif f.rain_level == Forecast.RAIN_ALMOST:
+            s = '{}時頃から雨が高確率で降ります。\n'.format(
+                f.rain_begin_time.hour
+            )
+
+        s += '\n【12時間後までの降水確率】\n'
+        for i in range(settings.FORECAST_LENGTH):
+            s += '{0:2d} 時: {1}%\n'.format(time.hour, f.pop[i])
+            time += dt.timedelta(hours=1)
+        max_pop_hour = 0
+        max_pop = 0
+        h = 0
+        for pop in f.pop:
+            if max_pop < pop:
+                max_pop = pop
+                max_pop_hour = h
+            h += 1
+        s += '\n最高降水確率: {}% ({} 時)\n'.format(
+            max_pop,
+            (f.rain_begin_time + dt.timedelta(hours=max_pop_hour)).hour)
+        return Responce(
+            message=s,
+            status=Responce.DETAIL
+        )
     elif 'ヘルプ' in message:
         return Responce(
-            message='今、傘がいるかどうか知りたい場合「傘いる？」'\
-            '特定の時刻で傘が必要な場合に通知させたい場合「通知 7:00」'\
-            '位置情報を設定または設定し直したい場合、位置情報を送信してください',
+            message='今、傘がいるかどうか知りたい場合「傘いる？」と聞いてください！\n\n'\
+            '天気予報の詳細が知りたい場合は「詳細」と聞いてくれればお教えします。\n' \
+            'また、特定の時刻で傘が必要な場合に通知させたい場合「通知 7:00」のように「通知」の後に通知を受け取りたい時刻を教えてください。\n'\
+            '位置情報を設定または設定し直したい場合、＋マークから位置情報を送信してください',
             status=Responce.HELP
         )
     else:

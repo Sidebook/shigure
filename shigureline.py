6878c7c0f50eac6641c59284d03685036f17742c
Wakimoto Ryu <sidebook37@gmail.com>
2017-07-24 02:45:00 +0900

    modified shigureline.py

--- shigureline.py
+++ shigureline.py
@@ -133,6 +133,8 @@ def add_user_setting(user_id, latitude=None, longitude=None, schedule=None):
             longitude,
             schedule
         ))
+    
+    print(json.dump(user_settings, indent=2))
 
 def load_user_settings():
     with open('usersettings.json') as f:
